ba73ece9d8f30edb9cc8ecaa27ce208bfc7519b8
husheric <ericliu@ac.c4q.nyc>
2018-04-05 21:03:58 -0400

    im an idiot

--- frontend/src/Components/Dashboard/Dashboard.jsx
+++ frontend/src/Components/Dashboard/Dashboard.jsx
@@ -1,57 +1,57 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-// import {
-// 		fetchAllActiveTasks, 
-// 		fetchAllActiveRecurringTasks, 
-// 		fetchAllVisibleNotes,
-// 		fetchAllApartmentGoals,
-//     fetchAllCompletedTasks
-// 	} from "./Actions/DashboardActions";
+import {
+		fetchAllActiveTasks, 
+		fetchAllActiveRecurringTasks, 
+		fetchAllVisibleNotes,
+		fetchAllApartmentGoals,
+    fetchAllCompletedTasks
+	} from "./Actions/DashboardActions";
 import { Grid } from "semantic-ui-react";
 import UpNextContainer from "./Containers/UpNextContainer";
 import CalendarContainer from "./Containers/CalendarContainer";
 import BulletinContainer from "./Containers/BulletinContainer";
 
-// const mapDispatchToProps = dispatch => {
-// 	return {
-//     fetchAllActiveTasks: aptid => {
-//       dispatch(fetchAllActiveTasks(aptid));
-//     },
-//     fetchAllActiveRecurringTasks: aptid => {
-//       dispatch(fetchAllActiveRecurringTasks(aptid));
-//     },
-//     fetchAllVisibleNotes: aptid => {
-//     	dispatch(fetchAllVisibleNotes(aptid));
-//     },
-//     fetchAllApartmentGoals: aptid => {
-//     	dispatch(fetchAllApartmentGoals(aptid));
-//     },
-//     fetchAllCompletedTasks: aptid => {
-//       dispatch(fetchAllCompletedTasks(aptid));
-//     }
-//   }
-// }
+const mapDispatchToProps = dispatch => {
+	return {
+    fetchAllActiveTasks: aptid => {
+      dispatch(fetchAllActiveTasks(aptid));
+    },
+    fetchAllActiveRecurringTasks: aptid => {
+      dispatch(fetchAllActiveRecurringTasks(aptid));
+    },
+    fetchAllVisibleNotes: aptid => {
+    	dispatch(fetchAllVisibleNotes(aptid));
+    },
+    fetchAllApartmentGoals: aptid => {
+    	dispatch(fetchAllApartmentGoals(aptid));
+    },
+    fetchAllCompletedTasks: aptid => {
+      dispatch(fetchAllCompletedTasks(aptid));
+    }
+  }
+}
 
-// const mapStateToProps = state => {
-//   return {
-//     tasks: state.App.tasks,
-//     recurringTasks: state.App.recurringTasks,
-//     notes: state.App.notes,
-//     goals: state.App.goals,
-//     completedTasks: state.App.completedTasks,
-//     successQueries: state.App.successQueries
-//   };
-// };
+const mapStateToProps = state => {
+  return {
+    tasks: state.Dashboard.tasks,
+    recurringTasks: state.Dashboard.recurringTasks,
+    notes: state.Dashboard.notes,
+    goals: state.Dashboard.goals,
+    completedTasks: state.Dashboard.completedTasks,
+    successQueries: state.Dashboard.successQueries
+  };
+};
 
 class Dashboard extends Component {
 	constructor(props) {
 		super(props);
 
-		// props.fetchAllActiveTasks(1);
-		// props.fetchAllActiveRecurringTasks(1);
-		// props.fetchAllVisibleNotes(1);
-		// props.fetchAllApartmentGoals(1);
-  //   props.fetchAllCompletedTasks(1);
+		props.fetchAllActiveTasks(1);
+		props.fetchAllActiveRecurringTasks(1);
+		props.fetchAllVisibleNotes(1);
+		props.fetchAllApartmentGoals(1);
+    props.fetchAllCompletedTasks(1);
 	}
 
   render() {
@@ -74,6 +74,5 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard;
 
-// export default connect(mapStateToProps)(Dashboard);
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
--- frontend/src/RootReducer.js
+++ frontend/src/RootReducer.js
@@ -1,8 +1,7 @@
-// import Homepage from "./Components/Homepage/Reducers/HomepageReducer";
-// import Dashboard from './Components/Dashboard/Reducers/DashboardReducer';
-// import Profile from './Components/UserProfile/Reducers/ProfileReducer';
-// import Apartment from "./Components/Apartment/Reducers/ApartmentReducers";
-import App from "./Reducers/reducers"
+import Homepage from "./Components/Homepage/Reducers/HomepageReducer";
+import Dashboard from './Components/Dashboard/Reducers/DashboardReducer';
+import Profile from './Components/UserProfile/Reducers/ProfileReducer';
+import Apartment from "./Components/Apartment/Reducers/ApartmentReducers";
 import { combineReducers } from "redux";
 
-export default combineReducers({ App });
+export default combineReducers({ Homepage, Dashboard, Profile, Apartment });
