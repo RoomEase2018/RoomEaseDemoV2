24df8323ef2eb9d5b6487c3acc71d093d0cfacd1
Ruben Profit <rubenprofit@ac.c4q.nyc>
2018-04-06 20:58:15 -0400

    fixed error from console.log(el.karma) issue

--- frontend/src/Components/UserProfile/Components/ApartmentGoals.jsx
+++ frontend/src/Components/UserProfile/Components/ApartmentGoals.jsx
@@ -22,7 +22,6 @@ class ApartmentGoals extends Component {
   	console.log(this.props.completedTasks)
   	if (this.props.completedTasks.length) {
   		this.apartmentKarma = this.props.completedTasks.reduce((acc, el) => {
-  			console.log(acc.karma, el.karma)
   			return acc + el.karma;
   		}, this.props.completedTasks[0].karma)
   	}
