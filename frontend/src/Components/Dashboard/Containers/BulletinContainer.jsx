5c4002b044e0db392519b562cf1c8c0d80863361
husheric <ericliu@ac.c4q.nyc>
2018-04-09 06:39:41 -0400

    bulletin notes + legend

--- frontend/src/Components/Dashboard/Containers/BulletinContainer.jsx
+++ frontend/src/Components/Dashboard/Containers/BulletinContainer.jsx
@@ -9,6 +9,22 @@ const mapStateToProps = state => {
     }
 }
 
+const legend = {
+    width: '200px',
+    height: '200px',
+    backgroundColor: '#77add7',
+    position: 'absolute',
+    top: '24px',
+    right: '25px'
+}
+
+const noteStyle = [
+    { top: '266px', left: '222px', fontFamily: "Schoolbell", fontSize: "1.5em", position: 'absolute', backgroundColor: 'pink' },
+    { top: '166px', left: '445px', fontFamily: "Schoolbell", fontSize: "1.5em", position: 'absolute', backgroundColor: 'lightblue' },
+    { top: '451px', left: '550px', fontFamily: "Schoolbell", fontSize: "1.5em", position: 'absolute', backgroundColor: 'violet' },
+    { top: '312px', left: '733px', fontFamily: "Schoolbell", fontSize: "1.5em", position: 'absolute', backgroundColor: 'lightpurple' }
+]
+
 class Bulletin extends Component {
     constructor(props) {
         super(props)
@@ -33,12 +49,20 @@ class Bulletin extends Component {
         return (
             <div className="bulletin">
                 <h1> Apartment Bulletin </h1>
+                <div style={legend}>
+                    <p>user1</p>
+                    <p>user2</p>
+                    <p>user3</p>
+                    <p>user4</p>
+                    <button>+ note</button>
+                </div>
                 {notes.map((note, i) => (
                     <Note
                         message={note.message}
                         index={i}
                         key={i}
                         hideNote={this.hideNote}
+                        noteStyle={noteStyle[i]}
                     />
                 ))}
             </div>
